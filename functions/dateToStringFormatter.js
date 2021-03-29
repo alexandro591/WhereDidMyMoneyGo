5d859685d7c06ad4f18f030fd97ef894670c48ab
alexandro591 <alexandrotapiaflores@gmail.com>
2021-03-29 00:49:04 -0500

    added moment.js to fix date issues

--- functions/dateToStringFormatter.js
+++ functions/dateToStringFormatter.js
@@ -1,14 +1,5 @@
+import moment from 'moment';
+
 export default function dateToStringFormatter(date) {
-    return date
-        .toLocaleTimeString('es-CA', {
-            year: 'numeric',
-            month: '2-digit',
-            day: '2-digit',
-            hour: '2-digit',
-            minute: '2-digit',
-            hour12: false,
-        })
-        .split(' ')
-        .join('T')
-        .replace(/\//g, '-');
+    return moment(date).format('yyyy-MM-DDTHH:mm');
 }
